b73fbe58c5f46fb8e9057a210be433688935f4d5
Mads <madi.gillis@gmail.com>
2022-07-27 17:13:23 -0700

    Add all the exercises i forgot to commit oops

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,14 @@
-const repeatString = function() {
-
+const repeatString = function(str, num) {
+    let newString = "";
+    if (num < 0) {
+        newString = "ERROR";
+    } else {
+        for (i = 0; i < num; i++) {
+            newString += str;
+    }
+    }
+    
+    return(newString);
 };
 
 // Do not edit below this line
