50e581adaf7e10c7c8b03f194a02c068f6dba901
Mads <madi.gillis@gmail.com>
2022-07-27 17:12:38 -0700

    Add all the exercises i forgot to commit oops

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,10 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(arr, ...args) {
+    for (const arg of args) {
+        if (arr.indexOf(arg) >= 0) {
+            arr.splice(arr.indexOf(arg), 1);
+        }
+      }
+      return arr;
 };
 
 // Do not edit below this line
