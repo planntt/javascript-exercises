0241aeffde55baf2b78b0a9ec6d256aac12c4fb4
Mads <madi.gillis@gmail.com>
2022-07-27 18:57:39 -0700

    Add solution to 07_tempConversion

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,13 @@
-const ftoc = function() {
-
+const ftoc = function(ftemp) {
+  const degC = (ftemp - 32) * (5/9);
+  const rounded = Math.round(degC * 10) / 10;
+  return rounded;
 };
 
-const ctof = function() {
-
+const ctof = function(ctemp) {
+  const degF = ctemp * (9/5) + 32;
+  const rounded = Math.round(degF * 10) / 10;
+  return rounded;
 };
 
 // Do not edit below this line
